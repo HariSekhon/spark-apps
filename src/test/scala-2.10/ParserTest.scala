f6cfb5a9f8f8c407639f561f195cc7e2540fff57
Hari Sekhon <harisekhon@gmail.com>
2015-06-16 14:29:32 +0000

    split out hdfs vs file tests and switched back to .equals to avoid confusion when switching back and forth between Java

--- src/test/scala-2.10/ParserTest.scala
+++ src/test/scala-2.10/ParserTest.scala
@@ -6,8 +6,10 @@ class ParserTest extends FunSuite {
   val parser = new Parser()
   
   test("parser strip_file_scheme should remove prefix hdfs://nameservice1:8020"){
-    assert(parser.strip_file_scheme("hdfs://nameservice1/path/to/dir") == "/path/to/dir")
-    assert(parser.strip_file_scheme("file:///path/to/dir") == "/path/to/dir")
+    assert(parser.strip_file_scheme("hdfs://nameservice1/path/to/dir").equals("/path/to/dir"))
+  }
+  test("parser strip_file_scheme should remove prefix file://"){
+    assert(parser.strip_file_scheme("file:///path/to/dir").equals("/path/to/dir"))
   }
   
 }
